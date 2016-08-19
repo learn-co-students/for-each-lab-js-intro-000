84d72ec933614919de60a3c0738a2caef65b0b0f
pwjablonski Jablonski <pwjablonski@gmail.com>
2016-08-19 16:39:35 +0000

    hi

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array){
+    debugger
+    
+    console.log(`${index}: ${element}`)
+
+}
+
+function iterate(callback){
+    array.forEach(callback)
+}
+
+function doToArray(array, callback){
+    debugger
+    array.forEach(callback)
+}
