ca0bc9c1da7db8b6be3ae4d4e4c2fe0f80f77f6d
Cindy Gao <cindygao@hotmail.com>
2016-08-28 20:42:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function iterativeLog(array){
-  array.forEach(console.log(`${index}: ${element}`))
+  array.forEach(function(element, index){
+    console.log(`${index}: ${element}`)
+  })
+  //array.forEach(console.log(`${index}: ${element}`))
 }
 
 function iterate(callback){
   var week = ["Monday", "Tuesday", "Wednesday", "Thursday"]
-  return week.forEach(callback)
+  week.forEach(callback)
+  return week
 }
 
 function doToArray(array, callback){
