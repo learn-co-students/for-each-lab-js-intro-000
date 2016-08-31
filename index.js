b6c1f279a800835f1e6ed802905985bd574e98cb
Shaw Li <shaw.s.li@gmail.com>
2016-08-31 01:25:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function iterativeLog(array) {
-  Array.forEach(element, index, array => {
-  console.log(`${index}: ${element}`)
+  array.forEach((element, index, array) =>{
+    console.log(`${index}: ${element}`)
+  })
+}
 
+function iterate(callback) {
+  var array = ['apple','orange','fruit'];
+  array.forEach(callback);
+  return(array)
 }
 
 function doToArray(array, callback) {
-
+  array.forEach(callback);
+  
 }
