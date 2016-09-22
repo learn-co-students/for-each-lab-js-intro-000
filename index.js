827cb73ccb58532787d077a1ab1502fbb5b00fa0
Tedman Hsu <twh26@cornell.edu>
2016-09-22 06:39:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+var index = 0
+  array.forEach(element => {
+    console.log(`${index}: ${element}`)
+    index++
+  })
+}
+
+function iterate(callback) {
+  var array = [1,2,3,4]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
