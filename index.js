817b5e733c0dcc65ec29f1044307fc360138d8cc
Jane Claire Remick <jdotcdot@hotmail.com>
2016-10-12 14:30:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function iterativeLog(array){
-  array.forEach(function(item, index) {
-    console.log(`${index}: ${item}`);
+  array.forEach(function(element, index) {
+    console.log(`${index}: ${element}`);
 });
+}
+
+function iterate(callback){
+  var thisArray = ["this","that","those","these"];
+  thisArray.forEach(callback);
+  return thisArray;
+}
+
+function doToArray(array,callback){
+  array.forEach(callback);
 }
