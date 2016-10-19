39c18b73afdc22c58de48f12c854a7111ce09463
Krs Lido <krslido@gmail.com>
2016-10-19 20:12:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function iterativeLog(array) {
+  array.forEach((name, l) => {
+    console.log(`${l}: {name}`)
+  })
+}
+
+
+function iterate(callback) {
+  const dogs = ["One", "Two", "Three"]
+
+  cats.forEach(callback)
+
+  return dogs
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
