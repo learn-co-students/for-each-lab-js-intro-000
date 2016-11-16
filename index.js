bd7239dd629a42a07615e3c2517326350fb5433a
Vanessa Jimenez <vjimenez127@gmail.com>
2016-11-16 06:03:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array){
+  array.forEach((element, index)=> {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback){
+  var anything=["Cali", 123, "ABC"]
+  anything.forEach(callback)
+    return anything
+}
+
+function doToArray(array, callback){
+  array.forEach((callback))
+}
