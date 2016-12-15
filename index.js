928f66bc4c4b14008231619c5e1c519abdb260cc
Amit Singh <amit_nyc@live.com>
2016-12-15 18:47:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function iterativeLog(array){
-  array.forEach(array => {
-      console.log(index:element)
+  array.forEach((array, index) => {
+      console.log(`${index}: ${array}`)
   })
+}
+function iterate(callback){
+  var array=[1,2,3]
+  array.forEach(callback)  
+  return array
+}
+function doToArray(array, callback){
+  array.forEach(callback)
 }
