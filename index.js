72eb9a8c2f3c96eb33cf900569002ff5c19a29e9
David Heisner <davidheis@hotmail.com>
2016-12-19 15:12:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array){
+    array.forEach(function(item){
+      console.log(`${item}: ${array[item]}`);
+
+    })
+  }
+
+
+// function iterate(callback){
+//   var arr = [];
+//   arr.forEach(callback);
+//   return arr;
+// }
+// function doToArray(){
+//
+// }
