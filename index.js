8773f07bea1f2ce8516a638d7ba0edfdd1ee8694
John Hwong <hammer.hwong@gmail.com>
2016-12-29 18:47:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function iterativeLog(array) {
+  array.forEach(callback)
+}
+
+
+function callback(element, index){
+  console.log(`${index}: ${element}`)
+}
+
+
+function iterate(callback){
+  var a = [1, 2, 3, 4, 5];
+  a.forEach(callback);
+  return a
+}
+
+function doToArray(array, callback){
+  array.forEach(callback);
+}
