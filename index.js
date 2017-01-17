560b4faa90a12a6268c321ff7bb151e5834e5a70
idith almog <almog.idith@gmail.com>
2017-01-17 22:51:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function logelement (element,index,array) {
   console.log(`${index}: ${element}`)
 }
-
 function iterativeLog(array) {
    array.forEach(logelement)
-      
-   }
-   }
+      }
+   
+function iterate(callback) {
+      var array=[1,1,1,1,1];
+      array.forEach(callback);
+      return array
+    }
+
+function doToArray (array,callback) {
+    array.forEach(callback)
+}
     
