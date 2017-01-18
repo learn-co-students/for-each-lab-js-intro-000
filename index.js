78489ea348e3c9b5e558f71608067771bc65bafd
Richard Burd <richardburdx@yahoo.com>
2017-01-18 20:33:32 +0000

    Done.

--- index.js
+++ index.js
@@ -12,24 +12,23 @@ function iterativeLog(array1){
 
 //Define a function, iterate,
 //that accepts a callback.
-function iterate(callback){
+function iterate(callback1){
 //Within the iterate() function,
 //you should initialize an array
 //it can contain anything you want.
-var array2 = [1, 2, 3]
+var array3 = [4, 5, 6];
 //Call .forEach() on this array,
 //passing the callback to .forEach()
-   array2.forEach(callback => {
+   array3.forEach(callback1);
 //Then return the array that
 //you initialized.
-     return array2)
-   }
+  return array3;
 }
 
 //Define a function, doToArray
 //that accepts an array and a callback.
-function doToArray(array, callback) {
+function doToArray(array4, callback2){
 //Call .forEach() on the array,
 //passing the callback as the forEach callback.
-  array.forEach(callback =>)
-}
+   array4.forEach(callback2)
+ }
