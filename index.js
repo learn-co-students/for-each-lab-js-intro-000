6e59a871e06a9baee46cbf9f9b82a6a48b19ead1
Peter Chicarielli <peterchicarielli@gmail.com>
2017-02-08 22:33:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  })
+};
+
+function iterate(callback){
+  var phones = ["iPhone","Samsung"];
+  phones.forEach(callback)
+    return phones;
+  }
+
+
+function doToArray(array, callback){
+  array.forEach(callback);
+}
