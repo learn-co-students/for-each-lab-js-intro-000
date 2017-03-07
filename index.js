046896ca5c7bf0c92158def5684da60cb2ff8828
Shamsher Singh <singhs94@hotmail.com>
2017-03-07 21:16:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function iterativeLog(arrays) {
+  arrays.forEach(function(element, index) {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var array = [1,2,3,4,5];
+
+  array.forEach(callback)
+
+  return array;
+
+}
+
+function doToArray(array,callback) {
+  array.forEach(callback);
+}
