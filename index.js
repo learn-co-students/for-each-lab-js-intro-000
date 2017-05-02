d1b1374288b4590b463f796b135d830c60666f55
Valerie Corvington Farrell <vcorvington@gmail.com>
2017-05-02 15:12:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function iterativeLog(array) {
+array.forEach((element, index) => {
+  console.log(`${index}: ${element}`)
+})
+}
+
+function iterate(callback) {
+ var numbers = [1, 2, 3]
+ numbers.forEach(callback)
+ return numbers;
+}
+
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
