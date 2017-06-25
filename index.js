07573eb294559ce9ac0639fab63b5ae8de72b199
Hoi Ki Yuen <cyhk116@gmail.com>
2017-06-25 00:00:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array)
+{
+  array.forEach((element, index) => {console.log(`${index}: ${element}`)})
+}
+
+function iterate(callback)
+{
+  var arr = ["banana", "fantasia"]
+  arr.forEach(callback)
+  return arr
+}
+
+function doToArray(array, callback)
+{
+  array.forEach(callback)
+}
