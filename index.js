1e75b82150ca2b2a0575ca39a184e64a93a050bb
Rebecca Kaplan <beccatess@gmail.com>
2017-07-13 04:05:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function iterativeLog(array){array.forEach((element,index) => {console.log(`${index}: ${element}`)
+})
+
+  }
+function iterate(callback){pets = ["dogs", "cats", "bats"];
+callback(pets);
+return pets; };
+function doToArray(array,callback){array.forEach(callback);};
