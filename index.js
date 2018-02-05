d1031ffe34aef5f0ffe505631e0f06650267b469
Jesen Tanadi <mail@jesentanadi.com>
2018-02-05 04:04:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+      console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  let myArray = ["hey!", 3, 5];
+
+  myArray.forEach(callback);
+  return myArray;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
