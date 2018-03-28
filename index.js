6ad7ea93f6c52070c4a2efbcc9a0e6d7c3ea12d6
Nick Wohnhas <npw7887@gmail.com>
2018-03-28 15:32:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array){
+  array.forEach((element,index,array) => {
+  console.log(`${index}: ${element}`);
+})
+}
+function iterate(callback){
+  var taco = ["pasta"]
+  taco.forEach(callback)
+    return taco
+  }
+  function doToArray(array,callback){
+    array.forEach(callback)
+  }  
