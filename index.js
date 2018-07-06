658b4b61e623920a26145abe2d307d8dd4f574d2
Martin Cervantes <cervantes.martine@gmail.com>
2018-07-06 17:40:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function iterativeLog() {
+function iterativeLog(array) {
+    array.forEach((element, index) => {
+        console.log(`${index}: ${element}`);
+    });
+}
 
+
+function iterate(func) {
+    var array = [2, 2, 3];
+    array.forEach(item => func(item));
 }
