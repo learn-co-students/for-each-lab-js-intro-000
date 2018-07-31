7d37e1585530db5e72caabc60ce6f89b747ab403
cmhollen <cmhollenkamp@gmail.com>
2018-07-31 19:26:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array){
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  });
+}
+function iterate(callback){
+  var array = [1, 3, 4];
+  array.forEach(callback);
+  return array;
+}
+function doToArray(array, callback){
+  array.forEach(callback);
+}
