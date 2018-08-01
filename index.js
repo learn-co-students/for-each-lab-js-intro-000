a0a572b3b5f50234f176249009e37706b6d61da0
joe fisher <jhanajon@gmail.com>
2018-08-01 21:45:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function iterativeLog(element, index, array) {
+ array.forEach(element => { console.log(`${index}: ${element}`) })
+}
+
+function iterate() {
+  
+}
+
+function doToArray() {
+  
+}
