98e3c29b24eea9768217b5e0a0e2b12c6d5681af
joe fisher <jhanajon@gmail.com>
2018-08-02 17:55:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function iterativeLog(element, index, array) {
- array.forEach(element => { console.log(`${index}: ${element}`) })
+
+var array = []
+function iterativeLog(array) {
+ array.forEach(function(element, index, array){
+ 	console.log(`${index}: ${element}`) })
 }
 
-function iterate() {
-  
+let labArray = ['joe', 'julius', 'roseanne', 'hana', 'jonathan']
+  function iterate(callback) {
+    labArray.forEach(callback); 
+      return labArray
 }
 
-function doToArray() {
-  
+function doToArray(array, callback) {
+  array.forEach(callback); {
+    return array
+  }
 }
+
