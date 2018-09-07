10ece2a89c14b01a6f2657edd94fb475f85b2b22
Linda Chang <lindachang186@gmail.com>
2018-09-07 21:16:56 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function iterativeLog(array){
     console.log(`${index}: ${element}`)
   })
 }
+
+function iterate(callback){
+  var array = [1,2,3,4];
+  array.forEach(callback);
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
