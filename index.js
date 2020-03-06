9af41b9a73a08f1a4b2e4b12f2ef5e2385bd9bd4
James Stamps Jr <jstampshere@gmail.com>
2020-03-06 20:15:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-function iterativeLog(array) {
+/*function iterativeLog(array) {
  array.forEach(`${index}: ${element}`);
 }
-return array;
+return iterativeLog();
+*/
+function iterativeLog(array) {
+ array.forEach((element,index)=> console.log(`${index}: ${element}`));
+}
+iterativeLog([1,2,3,4,5]);
 
+/*
+function iterate(callback, array= [1,2,3,4,5]) {
+  array.forEach(callback);
+
+return array; 
+} */
+
+function iterate(callback) {
+  array = [1,2,3];
+  array.forEach(callback);
+  
+  return array;
+}
+
+
+function doToArray(array, callback) {
+  array = [1,2,3];
+  array.forEach((callback));
+  
+}
+
+
+
+
+
+
+/*
+function iterate(callback, [array]) {
+  array.forEach((callback) => [array]);
+return array(callback);
+}
+*/
