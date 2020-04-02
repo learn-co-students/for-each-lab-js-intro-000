7eae364159cad2f8a854c8ce8e9c755cf82f1c58
Ian McKenzie <imckenzie777@gmail.com>
2020-04-02 05:27:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function iterativeLog(array) {
+	array.forEach(element => console.log(${index}: ${element}))
+}
+
+function iterate(callback) {
+	arr.forEach(callback() => new Array = [...kittens])
+}
+
+function doToArray (array, callback) {
+	callback.forEach()
+} 
