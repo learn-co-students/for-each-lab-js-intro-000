1b0aa17b640baafd8d3ed1083a7c921deeb505f1
Jacob Harvey <jacobharv00@gmail.com>
2021-02-08 23:06:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function iterativeLog(arr) {
+  arr.forEach((element, index) =>
+  console.log(`${index}: ${element}`)
+
+  )
+}
+
+function iterate(callback) {
+  arr = ['Jacob', 'Tash', 'Gracie']
+  arr.forEach(callback)
+  return arr
+
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+
+}
