3c7fd11adcb0e53f546ae0a11d87a157b1c9800f
mmmfang <mae.fang@gmail.com>
2021-05-08 21:15:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+  array.forEach(element, index, array) => {console.log(`${index}: ${element}`)}
+}
+
+* evens.forEach(even => {
+*   console.log(`${even} is not odd!`);
+* });
+
+function iterate(callback){
+  array = [1, 2, 4];
+  array.forEach(callback);
+  return array;
+}
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
